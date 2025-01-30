ae29914bc8399d53215b54afeeb2027fa096f055
Vertex <vertexportalhosting@gmail.com>
2025-01-30 07:48:28 +0500

    fix: minor change

--- src/app/demo/components/calendar/calendar.component.ts
+++ src/app/demo/components/calendar/calendar.component.ts
@@ -141,7 +141,6 @@ export class CalendarComponent {
           urgent: a.urgent,
         }
        });
-       console.log("events", this.events);
      });
   }
 
@@ -242,7 +241,7 @@ export class CalendarComponent {
     });
     doc.save(this.datePipe.transform(this.events[0]?.start, 'MMMM YYYY')+'.pdf');
   }
-  
+
   getEventTitles(day: any): string {
     console.log("day", day)
     return day.events.map((event: any) => event.title).join(', ');
