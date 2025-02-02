f34a23c4aa196334eeedddd21fb3c193b391a37d
Vertex <vertexportalhosting@gmail.com>
2025-02-03 02:57:19 +0500

    fix: forgotten hero

--- src/app/demo/components/cases/case-view/case-view.component.ts
+++ src/app/demo/components/cases/case-view/case-view.component.ts
@@ -633,6 +633,8 @@ export class CaseViewComponent {
     }
 
     formatDate(date) {
-        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+        const month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+        const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+        return date.getFullYear() + '-' + month + '-' + day;
     }
 }
