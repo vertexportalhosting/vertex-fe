8549146271b6cfde59935f07c6f6bca100ab4ce4
Vertex <vertexportalhosting@gmail.com>
2025-02-02 16:46:22 -0500

    fix: minor change

--- src/app/demo/components/calendar/calendar.component.ts
+++ src/app/demo/components/calendar/calendar.component.ts
@@ -248,6 +248,9 @@ export class CalendarComponent {
   }
 
   formatDate(date) {
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    // add 0 if less than 10 to month and date
+    const month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate();
+    return date.getFullYear() + '-' + month + '-' + day;
   }
 }
