960326f46bc852acf39f406c2ecb2605cb3eea58
Vertex <vertexportalhosting@gmail.com>
2025-06-24 21:37:00 +0500

    minor change

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -7,17 +7,11 @@ import { catchError, tap } from 'rxjs';
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
     emailPattern: RegExp = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
-    constructor(
-        public layoutService: LayoutService,
-        private fb: FormBuilder,
-        private router: Router,
-        private userControllerService: UserControllerService,
-        private route: ActivatedRoute
-    ) {}
+    constructor(public layoutService: LayoutService, private fb: FormBuilder, private router: Router, private userControllerService: UserControllerService, private route: ActivatedRoute) { }
     visible: boolean = false;
 
     public userForm: FormGroup;
@@ -28,58 +22,39 @@ export class LoginComponent implements OnInit {
 
     initializeUserForm(): void {
         this.userForm = this.fb.group({
-            email: [
-                '',
-                [
-                    Validators.required,
-                    Validators.email,
-                    Validators.pattern(this.emailPattern),
-                ],
-            ],
+            email: ['', [Validators.required, Validators.email, Validators.pattern(this.emailPattern)]],
             password: ['', Validators.required],
         });
     }
 
     onSubmit() {
         if (this.userForm.invalid) return;
-        this.userControllerService
-            .login({ body: this.userForm.value })
-            .pipe(
-                tap((data) => {
-                    if (data.token && data['role']) {
-                        localStorage.setItem('user', JSON.stringify(data));
-                        const returnUrl =
-                            this.route.snapshot.queryParams['returnUrl'] ||
-                            this.isDevice()
-                                ? '/case/list'
-                                : '/dashboard';
-                        this.router.navigate([returnUrl]);
-                    }
-                }),
-                tap(() => {
-                    this.userControllerService
-                        .whoAmI()
-                        .subscribe((data: any) => {
-                            const user =
-                                JSON.parse(localStorage.getItem('user')) || {};
-                            localStorage.setItem(
-                                'user',
-                                JSON.stringify({ ...user, ...data })
-                            );
-                        });
-                }),
-                catchError((err: any) => {
-                    alert('Invalid Credentials');
-                    localStorage.clear();
-                    throw err;
-                })
-            )
-            .subscribe();
+        this.userControllerService.login({ body: this.userForm.value }).pipe(tap((data) => {
+            if (data.token && data['role']) {
+                localStorage.setItem("user", JSON.stringify(data));
+                const returnUrl = this.route.snapshot.queryParams['returnUrl'];
+                if (returnUrl) {
+                    this.router.navigate(["/case/view/164"])
+                } else {
+                    this.isDevice() ? this.router.navigate(["/case/list"]) : this.router.navigate(["/dashboard"]);
+                }
+            }
+        })
+            , tap(() => {
+                this.userControllerService.whoAmI().subscribe((data: any) => {
+                    const user = JSON.parse(localStorage.getItem("user")) || {};
+                    localStorage.setItem("user", JSON.stringify({ ...user, ...data }))
+                });
+            })
+            , catchError((err: any) => {
+                alert('Invalid Credentials');
+                localStorage.clear();
+                throw err;
+            })).subscribe();
     }
 
     isDevice() {
-        return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(
-            navigator.userAgent
-        );
+        return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent);
     }
+
 }
