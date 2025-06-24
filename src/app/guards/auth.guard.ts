b98fb14042b0c036e6bcc8756abbb69eb2eef89c
Vertex <vertexportalhosting@gmail.com>
2025-06-24 21:16:15 +0500

    implement auth guard redirection

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,22 +1,35 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import {
+    ActivatedRouteSnapshot,
+    CanActivate,
+    Router,
+    RouterStateSnapshot,
+    UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
-    constructor(private router: Router) { }
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
+    constructor(private router: Router) {}
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ):
+        | Observable<boolean | UrlTree>
+        | Promise<boolean | UrlTree>
+        | boolean
+        | UrlTree {
         let data: any = localStorage.getItem('user');
         data = JSON.parse(data);
         if (data && data.token) {
             return true;
         }
-        
-        this.router.navigate(['/auth/login']);
+
+        this.router.navigate(['/auth/login'], {
+            queryParams: { returnUrl: state.url },
+        });
         return false;
     }
 }
